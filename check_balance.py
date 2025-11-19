6ac6c8046776cea0f965f318b213cf99a0297d55
Fenet Gizaw <simbani011@gmail.com>
2025-11-19 23:22:42 +0300

    Update check_balance.py

--- check_balance.py
+++ check_balance.py
@@ -8,5 +8,6 @@ pneumonia_path = r"C:\Users\melak\Downloads\Chest_Xray_Final\train\PNEUMONIA"
 normal_count = len(os.listdir(normal_path))
 pneumonia_count = len(os.listdir(pneumonia_path))
 
+
 print(f"NORMAL: {normal_count}, PNEUMONIA: {pneumonia_count}")
 print("Model input shape:", model.input_shape)
